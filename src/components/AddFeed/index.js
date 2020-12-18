9e1f0dc4a417d466d796d4b4856669a620e298f7
Engin Öztürk <engin.ozturk@bilyoner.com>
2020-12-18 17:01:48 +0300

    templating

--- src/components/AddFeed/index.js
+++ src/components/AddFeed/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { PropTypes } from 'prop-types';
-import { Form, Row, Col, Input, Button, message } from 'antd';
+import { Form, Row, Col, Input, Button, message, Upload, Divider } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 const AddFeed = ({closeAddFeed}) => {
   const [form] = Form.useForm();
+  const [showImagePicker, setShowImagePicker] = useState(false);
+  const [showVideoPlayer, setShowVideoPlayer] = useState(false);
+  const [videoSrcObject, setVideoSrcObject] = useState(null)
+
+  useEffect(() => {
+    initializeMedia();
+  }, []);
+
+  const initializeMedia = () => {
+    if (!('mediaDevices' in navigator)) {
+      navigator.mediaDevices = {};
+    };
+
+    if (!('getUserMedia' in navigator.mediaDevices)) {
+      navigator.mediaDevices.getUserMedia = constraints => {
+        const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented!'))
+        }
+
+        return new Promise((resolve, reject) => {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        })
+      }
+    }
+
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        setShowVideoPlayer(true);
+        setVideoSrcObject(stream);
+      })
+      .catch(err => {
+        setShowImagePicker(true);
+      });
+  };
 
   const onFinish = formData => {
     if (formData.title.trim() === '' || formData.location.trim() === '') {
@@ -27,7 +64,7 @@ const AddFeed = ({closeAddFeed}) => {
               })
               .then(function() {
                 message.info('Your Feed was saved for syncing!');
-                closeAddFeed();
+                onClose();
               })
               .catch(function(err) {
                 console.log('Background sync error: ', err);
@@ -38,11 +75,18 @@ const AddFeed = ({closeAddFeed}) => {
         // Have internet connection so, send data to backend
         axios.post('http://localhost:3004/posts', data).then(function(res) {
           message.success('Sent data!');
-          closeAddFeed();
+          onClose();
         });
       }
     }
   };
+
+  const onClose = () => {
+    setShowImagePicker(false);
+    setShowVideoPlayer(false);
+    setVideoSrcObject(null)
+    closeAddFeed();
+  }
 
   return (
     <div className='add-feed'>
@@ -51,6 +95,32 @@ const AddFeed = ({closeAddFeed}) => {
         form={ form }
         className='form form--vertical'
         onFinish={ onFinish }>
+
+        <div id='pick-image' className='text-center'>
+          
+          {
+            showVideoPlayer
+            &&
+            <video id='player' src={ videoSrcObject } autoPlay></video>
+          }
+
+          <canvas id='canvas' width='320' height='240'></canvas>
+
+          <Divider/>
+
+          <Button type='primary'>Capture</Button>
+
+          { 
+            showImagePicker
+            && <>
+            <h6 className='margin-top-20'>Picak an image instead</h6>
+            <Upload accept='image/*'>
+              <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            </Upload>
+          </> 
+          }
+
+        </div>
 
         <Row gutter={ 20 }>
           <Col span={ 24 }>
@@ -61,13 +131,13 @@ const AddFeed = ({closeAddFeed}) => {
 
           <Col span={ 24 }>
             <Form.Item label='Location' name='location' rules={[{ required: true, message: 'Location can not be empty!' }]}>
-              <Input />
+              <Input addonAfter={ <Button size='small'>Get Location</Button> } />
             </Form.Item>
           </Col>
 
           <Col span={ 24 } className='margin-top-20'>
             <Form.Item>
-              <Button type='default' className='margin-right-20' onClick={ closeAddFeed } danger>Cancel</Button>
+              <Button type='default' className='margin-right-20' onClick={ onClose } danger>Cancel</Button>
               <Button type='primary' htmlType='submit'>Save</Button>
             </Form.Item>
           </Col>
--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function() {
-      console.log('Service worker registered!');
-    })
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then(function() {
+//       console.log('Service worker registered!');
+//     })
+// }
