28f5c23d9babc0fcaaa7d3bc0a9188c07509b9b3
Engin Öztürk <engin.ozturk@bilyoner.com>
2020-12-22 17:45:07 +0300

    refactor

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-restricted-globals */
 
 var CACHE_STATIC_NAME = 'static-v3';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1';
 var STATIC_FILES = [
   '/',
   '/index.html',
@@ -35,31 +34,6 @@ var STATIC_FILES = [
   '/images/icons/icon-384x384.png',
   '/images/icons/icon-512x512.png'
 ];
-var MAX_CACHE_ITEMS = 20;
-
-function isInArray(string, array) {
-  var cachePath;
-  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-    // console.log('matched ', string);
-    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
-  } else {
-    cachePath = string; // store the full request (for CDNs)
-  }
-  return array.indexOf(cachePath) > -1;
-};
-
-function trimCache(cacheName, maxItems) {
-  caches.open(cacheName)
-    .then(function(cache) {
-      return cache.keys()
-        .then(function(keys) {
-          if (keys.length > maxItems) {
-            cache.delete(keys[0])
-              .then(trimCache(cacheName, maxItems));
-          };
-        });
-    });
-};
 
 self.addEventListener('install', function(event) {
   console.log('[SW] Installing service worker...');
@@ -77,154 +51,24 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activating service worker...');
 
-  // DELETE OLD CACHES
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[SW] Removing old cache: ', key);
-            return caches.delete(key)
-          }
-        }));
-      })
-  );
-
   return self.clients.claim();
 });
 
-// STRATEGY: BOTH: 1- Cache then Network / 2- Cache Only / 3- Cahce with Network fallback
-self.addEventListener('fetch', function(event) {
-  var apiUrl = 'http://localhost:3004/posts';
-
-  // 1- Cahce then Network
-  if (event.request.url.indexOf(apiUrl) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function(cache) {
-          return fetch(event.request)
-            .then(function(res) {
-              trimCache(CACHE_DYNAMIC_NAME, MAX_CACHE_ITEMS);
-              cache.put(event.request.url, res.clone());
-              return res;
-            });
-        })
-    );
-  } 
-
-  // 2- Cache Only
-  else if (isInArray(event.request.url, STATIC_FILES)) {
-    event.respondWith(
-      caches.match(event.request)
-    );
-  }
-
-  // 3- Cache with Network fallback
-  else {
-    event.respondWith(
-      // Retrieve data from cache if available
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            // Return from cahce
-            return response;
-          } else {
-            // Return from network
-            return fetch(event.request)
-
-              // DYNAMIC CACHING
-              // Cache every fetched data not list in static cache
-              .then(function(res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function(cache) {
-                    trimCache(CACHE_DYNAMIC_NAME, MAX_CACHE_ITEMS);
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function(err) {
-                console.log('Fetch error: ', err);
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function(cache) {
-                    if (event.request.headers.get('accept').includes('text/html')) {
-                      return cache.match('/fallback');
-                    };
-                  });
-              });
-          };
-      })
-    );
-  };
-});
-
-
-// STRATEGY: Cahce Only
-// Dangerous for dynamic content site
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//   );
-// });
-
-// STRATEGY: Network Only
-// Normal behavior (don't cache anything)
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     fetch(event.request)
-//   );
-// });
-
 // STRATEGY: Cahce with Network Fallback
-// self.addEventListener('fetch', function(event) {
-//   // console.log('[SW] Fetching something...', event);
-//   event.respondWith(
-
-//     // Retrieve data from cache if available
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           // Return from cahce
-//           return response;
-//         } else {
-//           // Return from network
-//           return fetch(event.request)
-
-//             // DYNAMIC CACHING
-//             // Cache every fetched data not list in static cache
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//             })
-//             .catch(function(err) {
-//               console.log('Fetch error: ', err);
-//               return caches.open(CACHE_STATIC_NAME)
-//                 .then(function(cache) {
-//                   return cache.match('/fallback');
-//                 })
-//             })
-//         }
-//       })
-//   );
-// });
-
-// STRATEGY: Network with cache fallback (Network first with dynamic caching)
-// Firstly request data from network
-// If request fails, return data from cache
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(function(res) {
-//         return caches.open(CACHE_DYNAMIC_NAME)
-//             .then(function(cache) {
-//               cache.put(event.request.url, res.clone());
-//               return res;
-//             })
-//       })
-//       .catch(function(err) {
-//         return caches.match(event.request);
-//       })    
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  // console.log('[SW] Fetching something...', event);
+  
+  event.respondWith(
+    // Retrieve data from cache if available
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          // Return from cahce
+          return response;
+        } else {
+          // Return from network
+          return fetch(event.request);
+        }
+      })
+  );
+});
